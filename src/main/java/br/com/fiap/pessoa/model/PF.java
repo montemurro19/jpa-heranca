f1ddca3e8f1921d5e16144402b85ad0a3c0d7fed
monts <montemurro.matheus@gmail.com>
2023-05-03 01:49:08 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/pessoa/model/PF.java
@@ -0,0 +1,8 @@
+package br.com.fiap.pessoa.model;
+
+public class PF extends Pessoa {
+
+    String CPF;
+    String RG;
+
+}
---
+++ src/main/java/br/com/fiap/pessoa/model/PJ.java
@@ -0,0 +1,8 @@
+package br.com.fiap.pessoa.model;
+
+public class PJ extends Pessoa{
+
+    String CNPJ;
+    String InscricaoEstadual;
+
+}
